1c3ce4d01555a5faa8861c56e87990bd4f94ac6e
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-07 16:13:18 +0000

    7bc6dcd5053b2d6128798325bbdf202dc14ef38c Release 4.2.1 (packaging error with 4.2.0)
    Sync to source repo @7bc6dcd5053b2d6128798325bbdf202dc14ef38c

--- js/fixedColumns.bootstrap4.mjs
+++ js/fixedColumns.bootstrap4.mjs
@@ -1,11 +1,10 @@
-
 /*! Bootstrap 4 integration for DataTables' FixedColumns
  * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-bs4';
-import DataTable from 'datatables.net-fixedcolumns';
+import 'datatables.net-fixedcolumns';
 
 
 
