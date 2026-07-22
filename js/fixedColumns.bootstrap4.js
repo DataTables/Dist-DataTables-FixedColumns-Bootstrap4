dc185ae726408d296ba9fd02e7532885253c67f2
dtbuild <dtbuild@rigil>
2026-07-22 14:30:45 +0000

    8049aa7a424619cd089c370ea54bf8e892c03407 Release 6.0.0 ready
    Sync to source repo @8049aa7a424619cd089c370ea54bf8e892c03407

--- js/fixedColumns.bootstrap4.js
+++ js/fixedColumns.bootstrap4.js
@@ -1,4 +1,4 @@
-/*! FixedColumns Bootstrap 4 styling 6.0.0-beta.1 for DataTables
+/*! FixedColumns Bootstrap 4 styling 6.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
